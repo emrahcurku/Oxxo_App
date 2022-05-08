4555be068107a720ad8da92f77a7173d2a9aff0b
emrahcurku <92750074+emrahcurku@users.noreply.github.com>
2022-05-09 00:51:45 +0300

    Add files via upload

---
+++ ShopApp/ShopApp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using ShopApp.Pages;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShopApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new TappedHome()); 
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ShopApp/ShopApp/TappedHome.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShopApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TappedHome : TabbedPage
+    {
+        public static TappedHome instance { get;  set; }
+        public TappedHome()
+        {
+            InitializeComponent();
+            instance = this;
+
+        }
+        
+    }
+}
